68546e226be7b3e1075095f903638df243995fa2
“Vilde <vilde@vevatne.no>
2017-10-18 13:23:50 -0700

    Create bare-bones component structure

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class StorePicker extends Component {
 	render() {
-		return <p>Hello</p>;
+		return (
+			<form className="store-selector">
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" />
+				<button type="submit">Visit Store</button>
+			</form>
+		);
 	}
 }
 
